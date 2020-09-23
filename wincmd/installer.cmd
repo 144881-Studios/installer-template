6eff3599835173e72294d01846535933db9bff50
cheny0y0 <66211043+cheny0y0@users.noreply.github.com>
2020-09-23 21:49:37 +0800

    Update installer.cmd

--- wincmd/installer.cmd
+++ wincmd/installer.cmd
@@ -1,2 +1,10 @@
 :Start
 echo Welcome to Install Wizard
+pause
+:Show “Press any key to continue…”
+md C:\Program Files\My Program
+:Replace your install address ↑.
+echo Hello World>>C:\Program Files\My Program\MyFile.txt
+:You can add more directories and files like this.
+echo Installation has benn completed
+pause>nul
