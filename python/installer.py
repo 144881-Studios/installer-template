7b287c052c6e5874b88391d5cc74c4f2524a18d2
cheny0y0 <66211043+cheny0y0@users.noreply.github.com>
2020-09-23 22:22:54 +0800

    Update installer.py

--- python/installer.py
+++ python/installer.py
@@ -1,6 +1,8 @@
 # Start
 from os import *
 print("Welcome to Install Wizard")
-raw = input(r"Please input the install directory(e.g.“C:\Program Files\”):")
-os.system("md "+raw+("\\"*(raw[-1]!="\\"))+"My Program\\")
-os.system("echo Hello World>>"+raw+("\\"*(raw[-1]!="\\"))+"My Program\\MyFile.txt"
+raw = input(r"Please input the install directory(e.g.“C:\Program Files\”):") # Choose install directory
+os.system("md "+raw+("\\"*(raw[-1]!="\\"))+"My Program\\") # Your Directory(Directories)
+os.system("echo Hello World>>"+raw+("\\"*(raw[-1]!="\\"))+"My Program\\MyFile.txt") # Your Files
+input("Done!")
+# End
